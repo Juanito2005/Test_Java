23a376eb86bc6948952052f254ddf102d24e0ffe
Juan Camilo Garcia Cuevas <juanca.cuevas.05@outlook.com>
2024-10-14 20:27:30 +0200

    test de sumar hecho

---
+++ src/test/java/org/factoriaf5/example/ExampleTest.java
@@ -0,0 +1,26 @@
+package org.factoriaf5.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void testSumar() {
+        Example example =  new Example();
+        // Given
+        int num1 = 35; // parametros
+        int num2 = 79;
+
+        // When - Cuando
+        int result = example.sumar(num1, num2);
+
+        // Then - Entonces
+        assertEquals(114, result);
+        assertTrue(result > 100);
+        assertFalse(result > 120);
+        assertNotNull(result);
+        assertInstanceOf(Integer.class, result);
+    }
+
+}
